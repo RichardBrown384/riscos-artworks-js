59f438280a7c26abbae3d143f73159e848b4ab5d
Ricki Brown <ricki.w.brown@gmail.com>
2022-07-10 15:39:39 +0200

    Move examples to top level

---
+++ examples/simple/001-triangle-fill-none.js
@@ -0,0 +1,31 @@
+/*
+Example: 001-triangle-fill-none
+
+Purpose:
+To demonstrate that !AWViewer crashes when a file doesn't contain a fill record
+ */
+
+const {
+  Builders: {
+    Artworks,
+    Lists,
+    List,
+  },
+} = require('../../src/artworks');
+
+const {
+  LAYER_FOREGROUND,
+  PATH_TRIANGLE,
+  WORK_AREA,
+} = require('../shared-objects');
+
+module.exports.moduleFilename = module.filename;
+module.exports.artworks = Artworks.builder()
+  .version(9)
+  .lists(
+    Lists.of(
+      List.of(LAYER_FOREGROUND, PATH_TRIANGLE),
+      List.of(WORK_AREA),
+    ),
+  )
+  .build();
---
+++ examples/simple/017-pentagram-fill-red-stroke-blue-stroke-width-3000-winding-none.js
@@ -0,0 +1,37 @@
+/*
+Example: 017-pentagram-fill-red-stroke-blue-stroke-width-3000-winding-none
+
+Purpose:
+To demonstrate that the default winding rule is even-odd
+ */
+
+const {
+  Builders: {
+    Artworks,
+    Lists,
+    List,
+  },
+} = require('../../src/artworks');
+
+const {
+  FILL_FLAT_RED,
+  STROKE_COLOUR_BLUE,
+  STROKE_WIDTH_3000,
+  LAYER_FOREGROUND,
+  PATH_PENTAGRAM,
+  WORK_AREA,
+} = require('../shared-objects');
+
+module.exports.moduleFilename = module.filename;
+module.exports.artworks = Artworks.builder()
+  .version(9)
+  .lists(
+    Lists.of(
+      List.of(FILL_FLAT_RED),
+      List.of(STROKE_COLOUR_BLUE),
+      List.of(STROKE_WIDTH_3000),
+      List.of(LAYER_FOREGROUND, PATH_PENTAGRAM),
+      List.of(WORK_AREA),
+    ),
+  )
+  .build();
