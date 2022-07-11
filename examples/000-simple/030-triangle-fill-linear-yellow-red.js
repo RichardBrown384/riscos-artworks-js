2db3679964b8c78081423d1335dcddfbba4d521d
Ricki Brown <ricki.w.brown@gmail.com>
2022-07-11 18:19:38 +0200

    Add basics for linear fills

---
+++ examples/000-simple/030-triangle-fill-linear-yellow-red.js
@@ -0,0 +1,31 @@
+/*
+Example: 030-triangle-fill-linear-yellow-red
+
+Purpose:
+To demonstrate the linear gradient fills !AWViewer
+ */
+
+const {
+  Builders: {
+    Artworks,
+    Lists,
+    List,
+  },
+} = require('../../src/artworks');
+
+const {
+  FILL_LINEAR_YELLOW_RED,
+  LAYER_FOREGROUND,
+  PATH_TRIANGLE,
+  WORK_AREA,
+} = require('../shared-objects');
+
+module.exports = Artworks.builder()
+  .lists(
+    Lists.of(
+      List.of(FILL_LINEAR_YELLOW_RED),
+      List.of(LAYER_FOREGROUND, PATH_TRIANGLE),
+      List.of(WORK_AREA),
+    ),
+  )
+  .build();
