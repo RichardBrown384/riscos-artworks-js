e4bceb1199a6aab0e3256bf325c2add9a903650f
Ricki Brown <ricki.w.brown@gmail.com>
2022-07-13 01:40:47 +0200

    Simple radial fill example

---
+++ examples/000-simple/031-pentagram-fill-radial-red-yellow.js
@@ -0,0 +1,35 @@
+/*
+Example: 031-pentagram-fill-radial-red-yellow.js
+
+Purpose:
+To demonstrate the radial gradient fills !AWViewer
+ */
+
+const {
+  Builders: {
+    Artworks,
+    Lists,
+    List,
+  },
+} = require('../../src/artworks');
+
+const {
+  FILL_RADIAL_RED_YELLOW,
+  STROKE_WIDTH_3000,
+  STROKE_COLOUR_BLUE,
+  LAYER_FOREGROUND,
+  PATH_PENTAGRAM,
+  WORK_AREA,
+} = require('../shared-objects');
+
+module.exports = Artworks.builder()
+  .lists(
+    Lists.of(
+      List.of(FILL_RADIAL_RED_YELLOW),
+      List.of(STROKE_COLOUR_BLUE),
+      List.of(STROKE_WIDTH_3000),
+      List.of(LAYER_FOREGROUND, PATH_PENTAGRAM),
+      List.of(WORK_AREA),
+    ),
+  )
+  .build();
