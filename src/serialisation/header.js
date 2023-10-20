3312534b31b1d6d1925ab1d6c9ee57fe63a2fcd7
RichardBrown384 <RichardBrown384@users.noreply.github.com>
2023-10-20 08:31:51 +0200

    Fix issue with header present check. (#164)

--- src/serialisation/header.js
+++ src/serialisation/header.js
@@ -1,9 +1,8 @@
 const Constants = require('../constants');
-
-const { readHeader } = require('../types/primitives');
+const { readArtworksHeader } = require('../types/structures');
 
 function isArtworksHeaderPresent(view) {
-  const { identifier, program } = readHeader(view);
+  const { identifier, program } = readArtworksHeader(view);
   return identifier === Constants.HEADER_IDENTIFIER && program === Constants.HEADER_PROGRAM;
 }
 
